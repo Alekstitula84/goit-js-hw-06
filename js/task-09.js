b812040c2ea76de349a3f846ebf1d53af0a03882
Alekstitula84 <alekstitula84@gmail.com>
2023-01-15 12:09:42 +0200

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const button = document.querySelector(".change-color");
+const spanColor = document.querySelector('.color')
+const bodyStyle = document.querySelector('body')
+// console.log(button);
+// console.log(spanColor);
+// console.log(bodyStyle);
+button.addEventListener('click', changeColor)
+
+function changeColor() {
+  bodyStyle.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = bodyStyle.style.backgroundColor;
+  console.log(spanColor.textContent);
 }
